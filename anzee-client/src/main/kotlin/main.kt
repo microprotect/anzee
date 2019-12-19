0acb8440cc40c320d2c774130e79d38c2bbdcdb6
noah <noahlog@naver.com>
2019-12-19 15:03:41 +0900

    Use request API
    Fetch remote JSON file using request API.
    
    See also:
    - https://developer.mozilla.org/en-US/docs/Web/API/Request
    - https://kotlinlang.org/api/latest/jvm/stdlib/org.w3c.dom/-window-or-worker-global-scope/fetch.html
    - https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.js/-j-s-o-n/parse.html

--- anzee-client/src/main/kotlin/main.kt
+++ anzee-client/src/main/kotlin/main.kt
@@ -1,6 +1,20 @@
 import kotlin.browser.document
+import kotlin.browser.window
+
+data class SampleResponse(val message: String)
+
+fun <T> fetchJSON(url: String, callback: (T) -> Unit) {
+    window.fetch(url).then { res ->
+        res.text().then { text ->
+            callback(JSON.parse<T>(text))
+        }
+    }
+}
 
 fun main() {
     val element = document.getElementById("app")
-    element?.innerHTML = "test"
+
+    fetchJSON<SampleResponse>("/data.json") {
+        element?.innerHTML = it.message
+    }
 }
